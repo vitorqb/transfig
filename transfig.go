56fe0c4c135c4ffd10c3381e30ccd25e0336e6e1
Vitor Barbosa <vitorqb@gmail.com>
2024-03-17 11:44:50 +0100

    feat: add nested states

--- transfig.go
+++ transfig.go
@@ -3,8 +3,48 @@
 
 package transfig
 
+import "github.com/google/uuid"
+
 // Key represents a key pointing to a value in the state
-type Key string
+type Key interface {
+	ExtractFrom(map[Key]interface{}) interface{}
+}
+
+// StringKey is a string representing a specific key in the state. When used in
+// a subscription, it will add a `key: value` pair to the subscription's arguments,
+// where `key` is the string and `value` is the value in the state.
+type KeyString string
+
+// ExtractFrom for a KeyString looks for the key in the state and returns its value.
+// If the key is not found, it returns nil. If the value is a nested state, it will
+// return a map with all values in the nested state.
+func (k KeyString) ExtractFrom(state map[Key]interface{}) interface{} {
+	value, exists := state[k]
+	if !exists {
+		return nil
+	}
+	if stateValue, ok := value.(*State); ok {
+		return Wildcard.ExtractFrom(stateValue.values)
+	}
+	return value
+}
+
+// keyWildcard is a special key that represents all keys in the state. When used in
+// a subscription, it will add a `key: value` pair to the subscription's arguments,
+// where `key` is wildcard itself and `value` is a map with all values in the state.
+// Nested states will be represented as maps as well.
+type keyWildcard struct{}
+
+// ExtractFrom for a keyWildcard returns the entire state.
+func (k keyWildcard) ExtractFrom(state map[Key]interface{}) interface{} {
+	out := make(map[Key]interface{})
+	for k := range state {
+		out[k] = k.ExtractFrom(state)
+	}
+	return out
+}
+
+var Wildcard = keyWildcard{}
 
 type SubscriptionCallback func(map[Key]interface{})
 
@@ -30,6 +70,9 @@ func (s *Subscription) Calls(callback SubscriptionCallback) *Subscription {
 
 func (s *Subscription) subscribedTo(key Key) bool {
 	for _, k := range s.keys {
+		if k == Wildcard {
+			return true
+		}
 		if k == key {
 			return true
 		}
@@ -40,11 +83,8 @@ func (s *Subscription) subscribedTo(key Key) bool {
 func (s *Subscription) notify(values map[Key]interface{}) {
 	args := make(map[Key]interface{})
 	for _, k := range s.keys {
-		if value, exists := values[k]; exists {
-			args[k] = value
-		} else {
-			args[k] = nil
-		}
+		value := k.ExtractFrom(values)
+		args[k] = value
 	}
 	for _, callback := range s.callbacks {
 		callback(args)
@@ -60,24 +100,48 @@ func NewSubscription(name string) *Subscription {
 }
 
 type State struct {
-	subscriptions map[string]*Subscription
-	values        map[Key]interface{}
+	subscriptions                 map[string]*Subscription
+	values                        map[Key]interface{}
+	nestedStatesSubscriptionNames map[Key]string
 }
 
 func (s *State) Set(key Key, value interface{}) {
+	if value == s.values[key] {
+		return
+	}
+	if oldVal, ok := s.values[key]; ok {
+		if oldStateVal, ok := oldVal.(*State); ok {
+			if subName, ok := s.nestedStatesSubscriptionNames[key]; ok {
+				oldStateVal.Unsubscribe(subName)
+			}
+		}
+	}
 	s.values[key] = value
+	if stateValue, ok := value.(*State); ok {
+		s.subscribeToNestedState(key, stateValue)
+	}
 	s.notifySubscribers(key)
 }
 
-func (s *State) Get(key Key) (value interface{}, exists bool) {
-	if value, exists = s.values[key]; exists {
-		return value, true
-	}
-	return nil, false
+func (s *State) Get(key Key) interface{} {
+	return key.ExtractFrom(s.values)
 }
 
 func (s *State) Subscribe(subscription *Subscription) {
 	s.subscriptions[subscription.name] = subscription
+}
+
+func (s *State) Unsubscribe(subscriptionName string) {
+	delete(s.subscriptions, subscriptionName)
+}
+
+func (s *State) subscribeToNestedState(key Key, state *State) {
+	subName := uuid.New().String()
+	sub := NewSubscription(subName).With(Wildcard).Calls(func(_ map[Key]interface{}) {
+		s.notifySubscribers(key)
+	})
+	s.nestedStatesSubscriptionNames[key] = subName
+	state.Subscribe(sub)
 }
 
 func (s *State) notifySubscribers(key Key) {
@@ -91,7 +155,8 @@ func (s *State) notifySubscribers(key Key) {
 // NewState creates a new state
 func NewState() *State {
 	return &State{
-		subscriptions: make(map[string]*Subscription),
-		values:        make(map[Key]interface{}),
+		subscriptions:                 make(map[string]*Subscription),
+		values:                        make(map[Key]interface{}),
+		nestedStatesSubscriptionNames: make(map[Key]string),
 	}
 }
--- transfig_test.go
+++ transfig_test.go
@@ -7,8 +7,12 @@ import (
 )
 
 var (
-	Name Key = "name"
-	Age  Key = "age"
+	Name         Key = KeyString("name")
+	Age          Key = KeyString("age")
+	Job          Key = KeyString("job")
+	Title        Key = KeyString("title")
+	Compensation Key = KeyString("compensation")
+	Ammount      Key = KeyString("ammount")
 )
 
 func DefaultState() *State {
@@ -18,20 +22,50 @@ func DefaultState() *State {
 	return x
 }
 
-func Test_SimpleGet(t *testing.T) {
+func Test_Get(t *testing.T) {
 	state := DefaultState()
-	name, exists := state.Get(Name)
-	assert.True(t, exists)
+	name := state.Get(Name)
 	assert.Equal(t, "John", name.(string))
 }
 
-func Test_GetNonExistentKey(t *testing.T) {
+func Test_Get_NonExistentKey(t *testing.T) {
 	state := DefaultState()
-	_, exists := state.Get("nonExistent")
-	assert.False(t, exists)
+	value := state.Get(KeyString("nonExistent"))
+	assert.Nil(t, value)
 }
 
-func TestSubscribe_OneVariable(t *testing.T) {
+func Test_Get_Wildcard(t *testing.T) {
+	state := DefaultState()
+	value := state.Get(Wildcard)
+	assert.Equal(t, map[Key]interface{}{Name: "John", Age: 30}, value)
+}
+
+func Test_Get_NestedState(t *testing.T) {
+	state := DefaultState()
+	subState := NewState()
+	state.Set(Job, subState)
+	subState.Set(Title, "Developer")
+	value := state.Get(Job)
+	assert.Equal(t, map[Key]interface{}{Title: "Developer"}, value)
+}
+
+func Test_Get_WildcardWithNestedState(t *testing.T) {
+	state := DefaultState()
+	subState := NewState()
+	state.Set(Job, subState)
+	subState.Set(Title, "Developer")
+	value := state.Get(Wildcard)
+	assert.Equal(t, map[Key]interface{}{
+		Name: "John",
+		Age:  30,
+		Job: map[Key]interface{}{
+			Title: "Developer",
+		}},
+		value,
+	)
+}
+
+func Test_Subscribe_OneVariable(t *testing.T) {
 	state := DefaultState()
 	callbackArgs := make(map[Key]interface{})
 	callback := func(args map[Key]interface{}) {
@@ -39,57 +73,158 @@ func TestSubscribe_OneVariable(t *testing.T) {
 	}
 	sub := NewSubscription("subName").With(Name).Calls(callback)
 	state.Subscribe(sub)
-	state.Set("name", "John")
-	expCallArgs := map[Key]interface{}{Name: "John"}
+	state.Set(Name, "Mike")
+	expCallArgs := map[Key]interface{}{Name: "Mike"}
 	assert.Equal(t, expCallArgs, callbackArgs)
 }
 
-func TestSubscribe_TwoVariable(t *testing.T) {
+func Test_Subscribe_TwoVariable(t *testing.T) {
 	state := DefaultState()
 	callbackArgs := make(map[Key]interface{})
 	callback := func(args map[Key]interface{}) {
 		callbackArgs = args
 	}
-	sub := NewSubscription("subName").With("name", "age").Calls(callback)
+	sub := NewSubscription("subName").With(Name, Age).Calls(callback)
 	state.Subscribe(sub)
-	state.Set("name", "Mike")
-	expCallArgs := map[Key]interface{}{"name": "Mike", "age": 30}
+	state.Set(Name, "Mike")
+	expCallArgs := map[Key]interface{}{Name: "Mike", Age: 30}
 	assert.Equal(t, expCallArgs, callbackArgs)
 }
 
-func TestSubscribe_MissingVariable(t *testing.T) {
+func Test_Subscribe_MissingVariable(t *testing.T) {
 	state := DefaultState()
 	callbackArgs := make(map[Key]interface{})
 	callback := func(args map[Key]interface{}) {
 		callbackArgs = args
 	}
-	sub := NewSubscription("subName").With("name", "NOT_A_KEY").Calls(callback)
+	missingKey := KeyString("NON_EXISTANT")
+	sub := NewSubscription("subName").With(Name, missingKey).Calls(callback)
 	state.Subscribe(sub)
-	state.Set("name", "Mike")
-	expCallArgs := map[Key]interface{}{"name": "Mike", "NOT_A_KEY": nil}
+	state.Set(Name, "Mike")
+	expCallArgs := map[Key]interface{}{Name: "Mike", missingKey: nil}
 	assert.Equal(t, expCallArgs, callbackArgs)
 }
 
-func TestSubscribe_TwoCallbacks(t *testing.T) {
+func Test_Subscribe_Wildcard(t *testing.T) {
+	state := DefaultState()
+	callbackArgs := make(map[Key]interface{})
+	callback := func(args map[Key]interface{}) { callbackArgs = args }
+	sub := NewSubscription("subName").With(Wildcard).Calls(callback)
+	state.Subscribe(sub)
+	state.Set(Name, "Mike")
+	expCallArgs := map[Key]interface{}{Wildcard: map[Key]interface{}{Name: "Mike", Age: 30}}
+	assert.Equal(t, expCallArgs, callbackArgs)
+}
+
+func Test_Subscribe_TwoCallbacks(t *testing.T) {
 	state := DefaultState()
 	callbackArgs1 := make(map[Key]interface{})
 	callback1 := func(args map[Key]interface{}) { callbackArgs1 = args }
 	callbackArgs2 := make(map[Key]interface{})
 	callback2 := func(args map[Key]interface{}) { callbackArgs2 = args }
-	sub := NewSubscription("subName").With("name").Calls(callback1).Calls(callback2)
+	sub := NewSubscription("subName").With(Name).Calls(callback1).Calls(callback2)
 	state.Subscribe(sub)
-	state.Set("name", "Mike")
-	expCallArgs := map[Key]interface{}{"name": "Mike"}
+	state.Set(Name, "Mike")
+	expCallArgs := map[Key]interface{}{Name: "Mike"}
 	assert.Equal(t, expCallArgs, callbackArgs1)
 	assert.Equal(t, expCallArgs, callbackArgs2)
 }
 
-func TestSubscribe_NotCalledIfNotSubscribed(t *testing.T) {
+func Test_Subscribe_NotCalledIfNotSubscribed(t *testing.T) {
 	state := DefaultState()
 	callbackCalled := false
 	callback := func(args map[Key]interface{}) { callbackCalled = true }
-	sub := NewSubscription("subName").With("name").Calls(callback)
+	sub := NewSubscription("subName").With(Name).Calls(callback)
 	state.Subscribe(sub)
-	state.Set("NOT_NAME", "John")
+	anotherKey := KeyString("NOT_NAME")
+	state.Set(anotherKey, "John")
 	assert.False(t, callbackCalled)
+}
+
+func Test_Subscribe_NestedState(t *testing.T) {
+	state := DefaultState()
+	subState := NewState()
+	state.Set(Job, subState)
+
+	callbackArgs := make(map[Key]interface{})
+	callback := func(args map[Key]interface{}) { callbackArgs = args }
+
+	sub := NewSubscription("subName").With(Job).Calls(callback)
+	state.Subscribe(sub)
+
+	subState.Set(Title, "Developer")
+
+	expCallArg := map[Key]interface{}{Job: map[Key]interface{}{Title: "Developer"}}
+	assert.Equal(t, expCallArg, callbackArgs)
+}
+
+func Test_Subscribe_TwoNestedState(t *testing.T) {
+	state := DefaultState()
+	jobState := NewState()
+	compensationState := NewState()
+	state.Set(Job, jobState)
+	jobState.Set(Compensation, compensationState)
+
+	callbackArgs := make(map[Key]interface{})
+	callback := func(args map[Key]interface{}) { callbackArgs = args }
+
+	sub := NewSubscription("subName").With(Job).Calls(callback)
+	state.Subscribe(sub)
+
+	compensationState.Set(Ammount, 1000)
+
+	expCallArg := map[Key]interface{}{Job: map[Key]interface{}{Compensation: map[Key]interface{}{Ammount: 1000}}}
+	assert.Equal(t, expCallArg, callbackArgs)
+}
+
+func Test_Subscribe_NestedStateWithWildcard(t *testing.T) {
+	state := DefaultState()
+	subState := NewState()
+	state.Set(Job, subState)
+	callCount := 0
+	callback := func(args map[Key]interface{}) { callCount++ }
+
+	sub := NewSubscription("subName").With(Wildcard).Calls(callback)
+	state.Subscribe(sub)
+
+	subState.Set(Title, "Developer")
+
+	assert.Equal(t, 1, callCount)
+}
+
+func Test_Set_SetSubstateTwice(t *testing.T) {
+	state := DefaultState()
+	subState := NewState()
+	state.Set(Job, subState)
+	state.Set(Job, subState)
+
+	callCount := 0
+	callback := func(args map[Key]interface{}) { callCount++ }
+
+	sub := NewSubscription("subName").With(Wildcard).Calls(callback)
+	state.Subscribe(sub)
+
+	subState.Set(Title, "Developer")
+
+	assert.Equal(t, 1, callCount)
+}
+
+func Test_Set_SetSubstateTwiceWithDifferentValues(t *testing.T) {
+	state := DefaultState()
+	subState1 := NewState()
+	state.Set(Job, subState1)
+	subState2 := NewState()
+	state.Set(Job, subState2)
+
+	callbcackArgs := make(map[Key]interface{})
+	callback := func(args map[Key]interface{}) { callbcackArgs = args }
+
+	sub := NewSubscription("subName").With(Job).Calls(callback)
+	state.Subscribe(sub)
+
+	subState1.Set(Title, "Developer")
+	subState2.Set(Title, "Manager")
+
+	expCallArgs := map[Key]interface{}{Job: map[Key]interface{}{Title: "Manager"}}
+	assert.Equal(t, expCallArgs, callbcackArgs)
 }

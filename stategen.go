1559a31ff34c238f1b464a6266dcc93c3ddccd49
Vitor Barbosa <vitorqb@gmail.com>
2024-04-05 16:44:33 +0200

    fix: fixes for stategen

--- stategen.go
+++ stategen.go
@@ -1,16 +1,36 @@
 package transfig
 
 import (
-	jen "github.com/dave/jennifer/jen"
 	"reflect"
+
+	jen "github.com/dave/jennifer/jen"
 )
 
 // GenNode represents a node in the state tree
 type GenNode map[string]interface{}
 
+// TypeWrapper represents a wrapper representing a type
+type TypeWrapper interface {
+	String() string
+}
+type StringType string
+func (s StringType) String() string { return string(s) }
+type ReflectType struct { t reflect.Type }
+func (r ReflectType) String() string { return r.t.String() }
+
+func NewTypeWrapper(s interface{}) TypeWrapper {
+	if v, ok := s.(string); ok {
+		return StringType(v)
+	}
+	if v, ok := s.(reflect.Type); ok {
+		return ReflectType{v}
+	}
+	return nil
+}
+
 // StateGen generates a new state tree
-func StateGen(rootNode GenNode, filepath string) error {
-	f := jen.NewFilePath(filepath)
+func StateGen(rootNode GenNode, package_ string, filepath string) error {
+	f := jen.NewFile(package_)
 	gen(Path{}, rootNode, f)
 	return f.Save(filepath)
 }
@@ -32,23 +52,25 @@ func gen(path Path, rootNode GenNode, f *jen.File) {
 			)
 			gen(append(path, KeyString(key)), nodeAsNode, f)
 		}
-		if nodeAsType, ok := node.(reflect.Type); ok {
-			jenPath := []jen.Code{}
-			for _, k := range path {
-				jenPath = append(jenPath, jen.Lit(string(k)))
-			}
-			jenPath = append(jenPath, jen.Lit(string(key)))
-
-			// Getter
-			f.Func().Params(jen.Id("s").Id(structName)).Id(key).Params().Id(nodeAsType.String()).Block(
-				jen.List(jen.Id("v"), jen.Id("_")).Op(":=").Id("s").Dot("wrappedState").Dot("GetNested").Call(jenPath...),
-				jen.Return(jen.Id("v").Assert(jen.Id(nodeAsType.String()))),
-			)
-
-			// Setter
-			f.Func().Params(jen.Id("s").Id(structName)).Id("Set" + key).Params(jen.Id("v").Id(nodeAsType.String())).Block(
-				jen.Id("s").Dot("wrappedState").Dot("SetNested").Call(jen.Qual("github.com/vitorqb/transfig", "Path").Values(jenPath...), jen.Id("v")),
-			)
+		nodeTypeWrapper := NewTypeWrapper(node)
+		if nodeTypeWrapper == nil {
+			continue
 		}
+		jenPath := []jen.Code{}
+		for _, k := range path {
+			jenPath = append(jenPath, jen.Lit(string(k)))
+		}
+		jenPath = append(jenPath, jen.Lit(string(key)))
+
+		// Getter
+		f.Func().Params(jen.Id("s").Id(structName)).Id(key).Params().Id(nodeTypeWrapper.String()).Block(
+			jen.List(jen.Id("v"), jen.Id("_")).Op(":=").Id("s").Dot("wrappedState").Dot("GetNested").Call(jenPath...),
+			jen.Return(jen.Id("v").Assert(jen.Id(nodeTypeWrapper.String()))),
+		)
+
+		// Setter
+		f.Func().Params(jen.Id("s").Id(structName)).Id("Set" + key).Params(jen.Id("v").Id(nodeTypeWrapper.String())).Block(
+			jen.Id("s").Dot("wrappedState").Dot("SetNested").Call(jen.Qual("github.com/vitorqb/transfig", "Path").Values(jenPath...), jen.Id("v")),
+		)
 	}
 }
